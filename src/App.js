cfb26f9f7c8868921522570995209f7ead61198b
ikarus-puneet <puneet.kumar@ikarus3d.com>
2023-04-12 15:37:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState } from 'react'
+import Navabar from './components/Navabar';
+import News  from './components/News' ;
+import{BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App =()=> {
+  const [progress,setProgress] = useState(0)  
+  
+    return (
+      <>
+      <Router>
+      <Navabar/>
+      <LoadingBar
+        color='#f11946'
+        progress={progress}
+      />
+      <Routes>
+        <Route exact path="/" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="general" pageSize={5} country="in" category="general"/>}>  </Route>
+        <Route exact path="/About" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="About" pageSize={5} country="in" category="About"/>}>  </Route>
+        <Route exact path="/Business" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="Business" pageSize={5} country="in" category="Business"/>}>  </Route>
+        <Route exact path="/Entertainment" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="Entertainment" pageSize={5} country="in" category="Entertainment"/>}>  </Route>
+        <Route exact path="/General" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="General" pageSize={5} country="in" category="General"/>}>  </Route>
+        <Route exact path="/Health" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="Health" pageSize={5} country="in" category="Health"/>}>  </Route>
+        <Route exact path="/Science" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="Science" pageSize={5} country="in" category="Science"/>}>  </Route>
+        <Route exact path="/Sports" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="Sports" pageSize={5} country="in" category="Sports"/>}>  </Route>
+        <Route exact path="/Technology" element={<News setProgress={setProgress} apiKey="8736697522154fc885927cb152ab78a5 " key="Technology" pageSize={5} country="in" category="Technology"/>}>  </Route>        
+      </Routes>
+      </Router>
+      </>
+    )
+  }
 
-export default App;
+  export default App
+
+        
